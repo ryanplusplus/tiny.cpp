8accf8ce5ab84e46c826037230161098c81a2660
Ryan Hartlage <ryanplusplus@gmail.com>
2020-07-14 19:25:58 -0400

    IAsyncI2c

---
+++ include/tiny/hal/IAsyncI2c.h
@@ -0,0 +1,39 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#ifndef IAsyncI2c_h
+#define IAsyncI2c_h
+
+#include <cstdint>
+
+namespace tiny
+{
+  class IAsyncI2c
+  {
+   public:
+    typedef void (*Callback)(void* context, bool success);
+
+   public:
+    auto virtual write(
+      uint8_t address,
+      bool prepare_for_restart,
+      const uint8_t* buffer,
+      uint16_t buffer_size,
+      Callback callback,
+      void* context) -> void = 0;
+
+    auto virtual read(
+      uint8_t address,
+      bool prepare_for_restart,
+      uint8_t* buffer,
+      uint16_t buffer_size,
+      Callback callback,
+      void* context) -> void = 0;
+
+    auto virtual reset() -> void = 0;
+  };
+}
+
+#endif
