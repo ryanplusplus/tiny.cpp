c79330ef90cf539d80ec9bb2f74e5d2209525168
Ryan Hartlage <ryanplusplus@gmail.com>
2020-07-13 21:17:26 -0400

    IGpio

---
+++ include/tiny/hal/IGpio.h
@@ -0,0 +1,29 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#ifndef IGpio_h
+#define IGpio_h
+
+#include <cstdint>
+
+namespace tiny
+{
+  class IUart
+  {
+   public:
+    enum class Direction : uint8_t
+    {
+      Input,
+      Output
+    };
+
+   public:
+    auto virtual set_direction(Direction direction) -> void;
+    auto virtual read() -> bool;
+    auto virtual write(bool state) -> void;
+  };
+}
+
+#endif
