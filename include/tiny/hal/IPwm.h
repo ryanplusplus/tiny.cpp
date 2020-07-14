bbdb632beb97a801becae1edc7028e9c0adf1dec
Ryan Hartlage <ryanplusplus@gmail.com>
2020-07-13 21:10:23 -0400

    IPwm; ISpi

---
+++ include/tiny/hal/IPwm.h
@@ -0,0 +1,25 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#ifndef IPwm_h
+#define IPwm_h
+
+#include <cstdint>
+
+namespace tiny
+{
+  class IPwm
+  {
+   public:
+    typedef uint16_t DutyCycle;
+    typedef uint16_t FrequencyHz;
+
+   public:
+    auto virtual set_duty_cycle(DutyCycle duty_cycle) -> void = 0;
+    auto virtual set_frequency(FrequencyHz frequency) -> void = 0;
+  };
+}
+
+#endif
---
+++ include/tiny/hal/ISpi.h
@@ -0,0 +1,23 @@
+/*!
+ * @file
+ * @brief Assumes chip select is managed by the client.
+ */
+
+#ifndef ISpi_h
+#define ISpi_h
+
+#include <cstdint>
+
+namespace tiny
+{
+  class ISpi
+  {
+   public:
+    auto virtual transfer(
+      const uint8_t* write_buffer,
+      uint8_t* read_buffer,
+      uint16_t buffer_size) -> void = 0;
+  };
+}
+
+#endif
