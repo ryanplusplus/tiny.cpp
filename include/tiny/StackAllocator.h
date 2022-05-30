e66438f327ecbcb9fe9cf991c936d66177188ff2
Ryan Hartlage <ryanplusplus@gmail.com>
2022-05-30 13:28:00 -0400

    Removed preprocessor magic

--- include/tiny/StackAllocator.h
+++ include/tiny/StackAllocator.h
@@ -6,6 +6,7 @@
 #ifndef StackAllocator_h
 #define StackAllocator_h
 
+#include <cstdint>
 #include <cstddef>
 
 namespace tiny {
@@ -24,15 +25,37 @@ namespace tiny {
       _allocate_aligned(size, context, reinterpret_cast<Callback>(callback));
     }
 
-    //  private:
+   private:
     typedef void (*Callback)(void* context, void* data);
 
+    template <size_t size>
+    static void worker(StackAllocator::Callback callback, void* context)
+    {
+      uint64_t data[size / sizeof(uint64_t)];
+      callback(context, data);
+    }
+
+    struct Worker {
+      size_t size;
+      void (*worker)(StackAllocator::Callback callback, void* context);
+    };
+
+    static constexpr Worker workers[] = {
+      {8, worker<8>},
+      {16, worker<16>},
+      {32, worker<32>},
+      {64, worker<64>},
+      {128, worker<128>},
+      {256, worker<256>},
+    };
+
    private:
-    static auto _allocate_aligned(
-      size_t size,
-      void* context,
-      Callback callback)
-      -> void;
+    static auto
+      _allocate_aligned(
+        size_t size,
+        void* context,
+        Callback callback)
+        -> void;
   };
 }
 
--- test/StackAllocator.cpp
+++ test/StackAllocator.cpp
@@ -3,40 +3,10 @@
  * @brief
  */
 
-#include <cstdint>
 #include "tiny/StackAllocator.h"
 #include "tiny/utils.h"
 
 using namespace tiny;
-
-#define define_worker(_size)                                                   \
-  static void worker_##_size(StackAllocator::Callback callback, void* context) \
-  {                                                                            \
-    uint64_t data[_size / sizeof(uint64_t)];                                   \
-    callback(context, data);                                                   \
-  }                                                                            \
-  typedef int dummy##_size
-
-define_worker(8);
-define_worker(16);
-define_worker(32);
-define_worker(64);
-define_worker(128);
-define_worker(256);
-
-struct Worker {
-  size_t size;
-  void (*worker)(StackAllocator::Callback callback, void* context);
-};
-
-static const Worker workers[] = {
-  {8, worker_8},
-  {16, worker_16},
-  {32, worker_32},
-  {64, worker_64},
-  {128, worker_128},
-  {256, worker_256},
-};
 
 auto StackAllocator::_allocate_aligned(
   size_t size,
